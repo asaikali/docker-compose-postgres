9699f6f46032ccb69b49de101f520e2e33f288a7
Adib Saikali <asaikali@gmail.com>
2020-08-02 16:51:34 -0400

    Developer friendly postgres configuration

---
+++ docker_postgres_init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE demo1
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE demo2
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
